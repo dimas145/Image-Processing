5fa126579bce8eb0c3bc19981da91f38c8cc651c
dimas145 <13518069@std.stei.itb.ac.id>
2022-02-16 22:02:49 +0700

    improve contrast_stretching algorithm

--- image_processing.m
+++ image_processing.m
@@ -2,8 +2,6 @@
 
 %% No 2
 figure,
-a = 30;
-b = 200;
 img_path = "img/Picture2.png";
 
 img = imread(img_path);
@@ -12,7 +10,7 @@ imshow(img);
 subplot(2, 2, 2);
 imhist(img);
 
-img_contrast = contrast_stretching(img_path, a, b);
+img_contrast = contrast_stretching(img_path);
 subplot(2, 2, 3);
 imshow(img_contrast);
 subplot(2, 2, 4);
@@ -23,29 +21,19 @@ imhist(img_contrast);
 %% No 4
 
 %% Functions
-function img = contrast_stretching(img_path, w1, w2)
+function img = contrast_stretching(img_path)
     img = imread(img_path);
-    rmin = min(img(:)); % TODO
-    rmax = max(img(:));
+    rmin = double(min(img(:)));
+    rmax = double(max(img(:)));
     L = 255;
-    a = w1 / rmin;
-    b = (w2 - w1)/(rmax - rmin);
-    g = (L - w2)/(L - rmax);
 
     [x, y, z] = size(img);
     for k = 1:z
         for i = 1:x
             for j = 1:y
-                if img(i,j,k) <= rmin
-                    r = img(i,j,k);
-                    img(i,j,k) = (a * r);
-                elseif img(i,j,k) > rmin && img(i,j,k) <= rmax
-                    r = img(i,j,k);
-                    img(i,j,k) = (b * (r - rmin)) + w1;
-                else
-                    r = img(i,j,k);
-                    img(i,j,k) = (g * (r - rmax)) + w2;
-                end
+                r = double(img(i, j, k));
+                s = L * ((r - rmin) / (rmax - rmin));
+                img(i, j, k) = s;
             end
         end
     end
